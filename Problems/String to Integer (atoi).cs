398b9eba8215aa99ba9408ad84d611139578ca50
Michael Mena <HalElt@live.com>
2024-08-02 15:47:13 -0400

    Add project files.

---
+++ Problems/String to Integer (atoi).cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    public partial class Solution
+    {
+        public int MyAtoi(string s)
+        {
+            short sign = 1;
+
+            int sum = 0;
+            ushort j = 0;
+
+            bool readingNum = false;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                char c = s[i];
+
+                int code = c;
+
+                if (code == 32 && !readingNum) // space
+                    continue;
+
+                if (code == 45 && sign == 1)
+                {// negative
+                    if (readingNum)
+                    {
+                        break;
+                    }
+                    sign = -1;
+                    readingNum = true;
+                    continue;
+                }
+
+                if (code == 43)
+                {
+                    if (readingNum)
+                    {
+                        break;
+                    }
+                    sign = 1;
+                    readingNum = true;
+                    continue;
+                }
+
+                int c_num = code - 48;
+                //return c_num;
+
+                if (c_num < 0 || c_num > 9)// is number
+                    break;
+
+                try
+                {
+                    sum = checked(c_num + sum * 10);
+                }
+                catch (OverflowException ex)
+                {
+                    sum = int.MaxValue;
+
+                    if (sign == -1)
+                    {
+                        sum += 1;
+                    }
+
+                    break;
+                }
+
+                readingNum = true;
+                if (sum != 0)
+                {
+                    j++;
+                }
+            }
+
+            return sum * sign;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using LeetCode.Problems;
+
+Solution s = new Solution();
+
+
+Console.WriteLine(s.MyAtoi("21474836460"));
