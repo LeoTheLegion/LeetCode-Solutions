c4b58d065256fcd6ee84f9e75dcc0e2378149e67
Michael Mena <HalElt@live.com>
2024-08-02 23:54:04 -0400

    solved

---
+++ Problems/Palindrome Number.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    public partial class Solution
+    {
+        public bool IsPalindrome(int x)
+        {
+            string s = x.ToString();
+
+            for (int i = 0; i < s.Length / 2; i++)
+            {
+                int opps = s.Length - 1 - i;
+                if (s[i] != s[opps])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
