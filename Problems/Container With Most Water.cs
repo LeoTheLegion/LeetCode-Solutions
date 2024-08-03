1b158af8a7b0a8c06171104f0d675d033167beef
Michael Mena <HalElt@live.com>
2024-08-03 15:49:14 -0400

    solved

---
+++ Problems/Container With Most Water.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    public partial class Solution
+    {
+        public int MaxArea(int[] height)
+        {
+
+            int MaxArea = 0;
+
+            int l = 0;
+            int r = height.Length - 1;
+
+            while (l < r)
+            {
+                bool leftHeavy = (height[l] > height[r]);
+                int minHeight = (leftHeavy) ? height[r] : height[l];
+
+                int area = minHeight * (r - l);
+
+                if (MaxArea < area)
+                {
+                    MaxArea = area;
+                }
+
+                if (leftHeavy)
+                {
+                    r--;
+                }
+                else
+                {
+                    l++;
+                }
+            }
+
+            return MaxArea;
+        }
+    }
+}
