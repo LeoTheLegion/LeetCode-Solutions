9c922424017b8beb15427fd9ab6bbd4828bfddd0
Michael Mena <HalElt@live.com>
2024-08-08 19:13:58 -0400

    Add Helper class with methods for linked list operations
    Added a new `Helper` class in the `LeetCode` namespace.
    The `Helper` class includes two static methods:
    - `CreateList(int[] values)`: Creates a linked list from an array.
    - `PrintList(ListNode list)`: Prints elements of a linked list.
    
    Included necessary `using` directives for various namespaces.

---
+++ Helper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode.Problems;
+
+namespace LeetCode
+{
+    public class Helper
+    {
+        public static ListNode CreateList(int[] values)
+        {
+            ListNode head = new ListNode(values[0]);
+            ListNode current = head;
+
+            for (int i = 1; i < values.Length; i++)
+            {
+                current.next = new ListNode(values[i]);
+                current = current.next;
+            }
+
+            return head;
+        }
+
+        public static void PrintList(ListNode list)
+        {
+            while (list != null)
+            {
+                Console.Write(list.val + " ");
+                list = list.next;
+            }
+        }
+    }
+}
