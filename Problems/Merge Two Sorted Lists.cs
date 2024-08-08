5dfddb69b735745a57862aa70c0bd8362a43a3ac
Michael Mena <HalElt@live.com>
2024-08-08 19:14:03 -0400

    Solved

---
+++ Problems/Merge Two Sorted Lists.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    /**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+    public partial class Solution
+    {
+        public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+        {
+
+            if (list1 == null) return list2;
+            if (list2 == null) return list1;
+
+            if (list1.val > list2.val) return MergeTwoLists(list2, list1);
+
+            ListNode start = list1;
+            ListNode tail = start;
+
+            while (start != null)
+            {
+
+                if (list2 == null) return list1;
+
+                int min = start.val;
+                int max = (start.next != null) ? start.next.val : 100;
+
+                if (min <= list2.val && list2.val <= max)
+                {
+                    ListNode insert = list2;
+
+                    list2 = list2.next;
+
+                    ListNode end = start.next;
+                    start.next = insert;
+                    insert.next = end;
+
+                    tail = insert;
+                    start = insert;
+                    continue;
+                }
+                tail = start;
+                start = start.next;
+            }
+
+            tail.next = list2;
+
+            return list1;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using LeetCode;
 using LeetCode.Problems;
 
 Solution s = new Solution();
 
 
-Console.WriteLine(s.IsValid("(])"));
+ListNode l1 = Helper.CreateList(new int[] { 1, 2, 4 });
+ListNode l2 = Helper.CreateList(new int[] { 1, 3, 4 });
+
+//Helper.PrintList(s.MergeTwoLists(l1, l2));
+
+Console.WriteLine(s.MergeTwoLists(l1, l2));
