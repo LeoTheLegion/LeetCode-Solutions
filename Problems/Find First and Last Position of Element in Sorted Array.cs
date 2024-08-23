81debf94b464de99c31ef2307b3de1da7d294694
Michael Mena <HalElt@live.com>
2024-08-23 16:47:51 -0400

    solved

---
+++ Problems/Find First and Last Position of Element in Sorted Array.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    public partial class Solution
+    {
+        public int[] SearchRange(int[] nums, int target)
+        {
+            int l = 0;
+            int r = nums.Length - 1;
+
+            if (l == r && nums[l] == target)
+                return new int[] { 0, 0 };
+
+            while (l < r)
+            {
+                if (nums[l] < target)
+                {
+                    l++;
+                }
+
+                if (nums[r] > target)
+                {
+                    r--;
+                }
+
+                if (nums[l] == nums[r] && nums[l] == target)
+                {
+                    return new int[] { l, r };
+                }
+            }
+
+            return new int[] { -1, -1 };
+        }
+    }
+}
