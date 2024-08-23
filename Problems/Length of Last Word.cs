014b8c053268abcf0d0984a90b8776ef326598b1
Michael Mena <HalElt@live.com>
2024-08-22 20:21:00 -0400

    solved

---
+++ Problems/Length of Last Word.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    public partial class Solution
+    {
+        public int LengthOfLastWord(string s)
+        {
+
+            int i = s.Length;
+
+            while (i > 0)
+            {
+                if (s[i - 1] == ' ')
+                {
+                    i--;
+                    continue;
+                }
+                break;
+            }
+
+            int j = i;
+
+            while (j > 0)
+            {
+                if (s[j - 1] != ' ')
+                {
+                    j--;
+                    continue;
+                }
+                break;
+            }
+
+            return i - j;
+        }
+    }
+}
