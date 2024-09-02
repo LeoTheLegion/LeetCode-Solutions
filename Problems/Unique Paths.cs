5376178685cbf2184460c04302a841741b1c02cc
Michael Mena <HalElt@live.com>
2024-09-02 13:04:12 -0400

    solved

---
+++ Problems/Unique Paths.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    public partial class Solution
+    {
+        public int UniquePaths(int m, int n)
+        {
+            int[] grid = new int[m * n];
+
+            int start = 0;
+            int end = grid.Length - 1;
+
+            //implement Depth First Search Algorithm
+            return DFS(grid, m, n, start, end);
+
+            int DFS(int[] grid, int m, int n, int start, int end)
+            {
+                if (start == end)
+                {
+                    return 1;
+                }
+
+                if (grid[start] > 0)
+                {
+                    return grid[start];
+                }
+
+                int count = 0;
+
+                if (start + 1 < m * n && (start + 1) % m != 0)
+                {
+                    count += DFS(grid, m, n, start + 1, end);
+                }
+
+                if (start + m < m * n)
+                {
+                    count += DFS(grid, m, n, start + m, end);
+                }
+
+                grid[start] = count;
+
+                return count;
+            }
+        }
+    }
+}
