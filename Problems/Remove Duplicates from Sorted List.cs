00b6e9e55478475053b6130917979a9a37f177a1
Michael Mena <HalElt@live.com>
2024-09-03 12:11:44 -0400

    solved

---
+++ Problems/Remove Duplicates from Sorted List.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    public partial class Solution
+    {
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+            ListNode current = head;
+
+            while (current != null && current.next != null)
+            {
+                if (current.val == current.next.val)
+                {
+                    current.next = current.next.next;
+                }
+                else
+                {
+                    current = current.next;
+                }
+            }
+
+            return head;
+        }
+    }
+}
