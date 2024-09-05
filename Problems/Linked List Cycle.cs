d7ade30a7f11774fce4b9f5efedae7d66b7961eb
Michael Mena <HalElt@live.com>
2024-09-05 19:38:37 -0400

    solved

---
+++ Problems/Linked List Cycle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    public partial class Solution
+    {
+        public bool HasCycle(ListNode head)
+        {
+            HashSet<ListNode> set = new HashSet<ListNode>();
+
+            while (head != null)
+            {
+                if (set.Contains(head))
+                {
+                    return true;
+                }
+
+                set.Add(head);
+                head = head.next;
+            }
+
+            return false;
+        }
+    }
+}
