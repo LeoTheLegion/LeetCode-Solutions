b05c07c369483cb4b6c70bc08109e429e4cde5d2
Michael Mena <HalElt@live.com>
2024-09-06 18:33:19 -0400

    solved

---
+++ Problems/Excel Sheet Column Number.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    public partial class Solution
+    {
+        public int TitleToNumber(string columnTitle)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < columnTitle.Length; i++)
+            {
+                char c = columnTitle[i];
+
+                int v = c - 'A' + 1;
+                sum = sum * 26 + v;
+            }
+
+            return sum;
+        }
+    }
+}
