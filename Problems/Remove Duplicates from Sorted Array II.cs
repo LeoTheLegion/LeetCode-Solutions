a5099cf50a64c08be9f95644dd50d66e02db72b9
Michael Mena <HalElt@live.com>
2024-09-07 11:35:13 -0400

    solved

---
+++ Problems/Remove Duplicates from Sorted Array II.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    public partial class Solution
+    {
+        public int RemoveDuplicates2(int[] nums)
+        {
+            int k = 0;
+            for(int i = 0; i < nums.Length; i++)
+            {
+                int x = nums[i];
+                int y = (i + 1 < nums.Length) ? nums[i + 1] : int.MaxValue - 1;
+                int z = (i + 2 < nums.Length) ? nums[i + 2] : int.MaxValue;
+                if (x == y && y != z)
+                {
+                    nums[k++] = nums[i];
+                }else if(x != y)
+                {
+                    nums[k++] = nums[i];
+                }
+            }
+
+            return k;
+        }
+    }
+}
