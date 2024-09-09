36e97d2c9113f9ecff3b6188dadb430c8076d850
Michael Mena <HalElt@live.com>
2024-09-09 13:49:06 -0400

    solved

---
+++ Problems/Reverse Linked List II.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    public partial class Solution
+    {
+        public ListNode ReverseBetween(ListNode head, int left, int right)
+        {
+            if (head == null || head.next == null || left == right)
+            {
+                return head;
+            }
+
+            ListNode[] nodesToReverse = new ListNode[right - left + 1];
+            int i = 0;
+
+
+            ListNode curr = head;
+            ListNode rotateLeft = null;
+            ListNode rotateRight = null;
+
+            // Find the nodes to reverse
+            while (curr != null)
+            {
+                // If we are at the node before the range of nodes to reverse
+                if (i == left - 2)
+                {
+                    rotateLeft = curr;
+                }
+                // If we are in the range of nodes to reverse
+                else if (i >= left - 1 && i <= right - 1)
+                {
+                    nodesToReverse[i - left + 1] = curr;
+                }
+                // If we are at the node after the range of nodes to reverse
+                else if( i == right)
+                {
+                    rotateRight = curr;
+                    break;
+                }
+
+                i++;
+                curr = curr.next;
+            }
+
+            // Link nodes in reverse
+            for (int j = nodesToReverse.Length - 1; j > 0; j--)
+            {
+                nodesToReverse[j].next = nodesToReverse[j - 1];
+            }
+
+            // Set Left Side
+            if(rotateLeft != null)
+                rotateLeft.next = nodesToReverse[nodesToReverse.Length - 1];
+            else
+                head = nodesToReverse[nodesToReverse.Length - 1];
+
+            // Set Right Side
+            nodesToReverse[0].next = rotateRight;
+
+            return head;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,40 +7,7 @@ Solution s = new Solution();
 
 ListNode[] listNodes = new ListNode[3];
 
-ListNode l1 = Helper.CreateList(new int[] { 1, 2 });
+ListNode l1 = Helper.CreateList(new int[] { 3, 5 });
 
-//Helper.PrintList(s.RotateRight(l1,1));
+Helper.PrintList(s.ReverseBetween(l1,1,2));
 
-//Helper.PrintList(s.ReverseKGroup(l1,2));
-
-//Helper.PrintListOfLists(s.FourSum(new int[] { 1000000000, 1000000000, 1000000000, 1000000000 }, -294967296));
-
-//Helper.PrintList(s.LetterCombinations('23'));
-
-string board = "[[ 5 ,  3 ,  . ,  . ,  7 ,  . ,  . ,  . ,  . ], [ 6 ,  . ,  . ,  1 ,  9 ,  5 ,  . ,  . ,  . ], [ . ,  9 ,  8 ,  . ,  . ,  . ,  . ,  6 ,  . ], [ 8 ,  . ,  . ,  . ,  6 ,  . ,  . ,  . ,  3 ], [ 4 ,  . ,  . ,  8 ,  . ,  3 ,  . ,  . ,  1 ], [ 7 ,  . ,  . ,  . ,  2 ,  . ,  . ,  . ,  6 ], [ . ,  6 ,  . ,  . ,  . ,  . ,  2 ,  8 ,  . ], [ . ,  . ,  . ,  4 ,  1 ,  9 ,  . ,  . ,  5 ], [ . ,  . ,  . ,  . ,  8 ,  . ,  . ,  7 ,  9 ]]";
-
-char[][] b = Helper.ConvertStringInto2DArray<char>(board);
-
-//Helper.Print2DArray(b);
-
-//Console.WriteLine(s.IsValidSudoku(b));
-
-//Console.WriteLine(s.SearchInsert(new int[] { 1, 3, 5, 6 },2));
-
-//Console.WriteLine(s.Multiply("123456789", "987654321"));
-
-//Console.WriteLine(s.MySqrt(8));
-
-int[][] intervals = new int[][]
-{ 
-    //[[6,10],[13,16],[19,19],[23,25],[34,39],[41,43],[49,51]]
-    new int[] { 6, 10 },
-    new int[] { 13, 16 },
-    new int[] { 19, 19 },
-    new int[] { 23, 25 },
-    new int[] { 34, 39 },
-    new int[] { 41, 43 },
-    new int[] { 49, 51 }
-};
-
-Helper.PrintListOfLists(s.Insert(intervals,new int[] { 27, 27 }));
