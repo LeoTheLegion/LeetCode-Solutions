ffc2cc0d3fc09ffd71bda026118b489be5c22478
Michael Mena <HalElt@live.com>
2024-09-10 10:02:54 -0400

    solved

---
+++ Problems/Search in Rotated Sorted Array II.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    public partial class Solution
+    {
+        public bool Search2(int[] nums, int target)
+        {
+            Array.Sort(nums);
+
+            int left = 0;
+            int right = nums.Length - 1;
+
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+
+                if (nums[mid] == target)
+                {
+                    return true;
+                }
+
+                if (nums[mid] < target)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+
+            return false;
+        }
+    }
+}
