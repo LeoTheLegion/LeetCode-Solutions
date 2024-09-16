e405f5bb94fb64fb46c5a3057bae5502f7e37439
Michael Mena <HalElt@live.com>
2024-09-16 09:56:56 -0400

    solved

---
+++ Problems/Linked List Cycle II.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    public partial class Solution
+    {
+        public ListNode DetectCycle(ListNode head)
+        {
+            Dictionary<ListNode, int> dict = new Dictionary<ListNode, int>();
+
+            ListNode current = head;
+
+            while (current != null)
+            {
+                if (dict.ContainsKey(current))
+                {
+                    return current;
+                }
+
+                dict.Add(current, 1);
+                current = current.next;
+            }
+
+            return null;
+        }
+    }
+}
