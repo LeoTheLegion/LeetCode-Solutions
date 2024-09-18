f3aba98d8ca0e50ca7c46c76a1230fadb7dfed4d
Michael Mena <HalElt@live.com>
2024-09-18 18:14:34 -0400

    solved

---
+++ Problems/Maximum Depth of Binary Tree.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    public partial class Solution
+    {
+        public int MaxDepth(TreeNode root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            return 1 + Math.Max(MaxDepth(root.left), MaxDepth(root.right));
+
+        }
+    }
+}
