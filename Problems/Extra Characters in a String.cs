45b187278df2d6cefb4205ea124e0ebff2c90ae3
Michael Mena <HalElt@live.com>
2024-09-23 14:53:19 -0400

    solved

---
+++ Problems/Extra Characters in a String.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    public partial class Solution
+    {
+        public int MinExtraChar(string s, string[] dictionary)
+        {
+            // Get the length of the input word and the dictionary
+            int inputWordLength = s.Length;
+            int DicLength = dictionary.Length;
+
+            // dp[i] is the minimum number of extra characters needed to form the first i characters of s
+            int[] dp = new int[inputWordLength + 1];
+            // Initialize dp
+            dp[0] = 0;
+            // Calculate dp
+            for (int i = 1; i <= inputWordLength; i++)
+            {
+                // Initialize dp[i] to be dp[i - 1] + 1
+                dp[i] = dp[i - 1] + 1;
+                // Check if we can form a word from the dictionary
+                for (int j = 0; j < DicLength; j++)
+                {
+                    // Get the length of the word
+                    int wordLength = dictionary[j].Length;
+
+                    // If the word is longer than i, skip it
+                    if(wordLength > i)
+                        continue;
+
+                    // Get the substring of s that ends at i
+                    var sub = s.Substring(i - wordLength, wordLength);
+
+                    // If the substring is not the same as the word, skip it
+                    if(sub != dictionary[j])
+                        continue;
+
+                    // Update dp[i] to be the minimum of dp[i] and dp[i - wordLength]
+                    dp[i] = Math.Min(dp[i], dp[i - wordLength]);
+                }
+            }
+            // Return the result
+            return dp[inputWordLength];
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,5 +9,10 @@ ListNode[] listNodes = new ListNode[3];
 
 ListNode l1 = Helper.CreateList(new int[] { 3, 5 });
 
-Helper.PrintList(s.ReverseBetween(l1,1,2));
+//Helper.PrintList(s.ReverseBetween(l1,1,2));
+
+Console.WriteLine(s.MinExtraChar("eglglxa", new string[]
+{
+    "rs","j","h","g","fy","l","fc","s","zf","i","k","x","gl","qr","qj","b","m","cm","pe","y","ei","wg","e","c","ll","u","lb","kc","r","gs","p","ga","pq","o","wq","mp","ms","vp","kg","cu"
+}));
 
