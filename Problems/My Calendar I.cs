4a35dce66931c7515a536098f2bb5c4227c97b99
Michael Mena <HalElt@live.com>
2024-09-26 14:30:56 -0400

    Solved

---
+++ Problems/My Calendar I.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    public class MyCalendar
+    {
+        private SortedList<int, int> calendar;
+
+        public MyCalendar()
+        {
+            calendar = new SortedList<int, int>();
+        }
+
+        public bool Book(int start, int end)
+        {
+            // If the calendar is empty, add the event
+            if (calendar.Count == 0)
+            {
+                calendar.Add(start, end);
+                return true;
+            }
+
+            // Check if the event overlaps with any existing events
+            foreach (var entry in calendar)
+            {
+                if (start < entry.Value && end > entry.Key)
+                {
+                    return false;
+                }
+            }
+
+            // Add the event to the calendar
+            calendar.Add(start, end);
+            return true;
+        }
+    }
+}
