c8db04dd3b0b6820beffae20afd704da67e0db9e
Michael Mena <HalElt@live.com>
2024-10-07 14:17:40 -0400

    solved

---
+++ Problems/Minimum String Length After Removing Substrings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    public partial class Solution
+    {
+        public int MinLength(string s)
+        {
+            while (s.IndexOf("AB") + s.IndexOf("CD") >= -1)
+            {
+                s = s.Replace("AB", "").Replace("CD", "");
+            }
+
+            return s.Length;
+        }
+    }
+}
