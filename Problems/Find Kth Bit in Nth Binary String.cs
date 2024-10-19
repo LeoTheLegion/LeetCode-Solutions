86ee77cddc19bb37679f4738d9f85499400cc34c
Michael Mena <HalElt@live.com>
2024-10-19 11:31:01 -0400

    solved

---
+++ Problems/Find Kth Bit in Nth Binary String.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    public partial class Solution
+    {
+        public char FindKthBit(int n, int k)
+        {
+            // If n is 1, return 0
+            if (n == 1)
+            {
+                return '0';
+            }
+
+            // Calculate the length of the string
+            var len = (int)Math.Pow(2, n) - 1;
+
+            // If k is half the length, return 1
+            if (k == len / 2 + 1)
+            {
+                return '1';
+            }
+
+            // If k is less than half the length, return the kth bit
+            if (k < len / 2 + 1)
+            {
+                return FindKthBit(n - 1, k);
+            }
+
+            // If k is greater than half the length, return the complement of the kth bit
+            return FindKthBit(n - 1, len - k + 1) == '0' ? '1' : '0';
+        }
+    }
+}
