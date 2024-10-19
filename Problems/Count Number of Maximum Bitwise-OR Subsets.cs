55845d3dd3999d58f2e7ce0640dcdb80517b5cd7
Michael Mena <HalElt@live.com>
2024-10-19 11:30:57 -0400

    solved

---
+++ Problems/Count Number of Maximum Bitwise-OR Subsets.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LeetCode.Problems
+{
+    public partial class Solution
+    {
+        public int CountMaxOrSubsets(int[] nums)
+        {
+            int maxOr = 0;
+            foreach (var num in nums)
+            {
+                maxOr |= num;
+            }
+
+            return CountSubsets(nums, 0, 0, maxOr);
+
+            int CountSubsets(int[] nums, int index, int currentOr, int maxOr)
+            {
+                if (index == nums.Length)
+                {
+                    return currentOr == maxOr ? 1 : 0;
+                }
+
+                // Include the current element in the subset
+                int include = CountSubsets(nums, index + 1, currentOr | nums[index], maxOr);
+
+                // Exclude the current element from the subset
+                int exclude = CountSubsets(nums, index + 1, currentOr, maxOr);
+
+                return include + exclude;
+            }
+        }
+    }
+}
