32e376b937b69f4ef5ac471153dbbb0ad0c0f96e
Michael Mena <HalElt@live.com>
2024-10-20 19:56:09 -0400

    solved

---
+++ Problems/Parsing A Boolean Expression.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    public partial class Solution
+    {
+        public bool ParseBoolExpr(string expression)
+        {
+            var stack = new Stack<char>();
+            foreach (var c in expression)
+            {
+                if (c == ')')
+                {
+                    var list = new List<char>();
+                    while (stack.Peek() != '(')
+                    {
+                        list.Add(stack.Pop());
+                    }
+                    stack.Pop(); // Pop '('
+                    var op = stack.Pop(); // Pop operator
+                    if (op == '!')
+                    {
+                        stack.Push(list[0] == 't' ? 'f' : 't');
+                    }
+                    else if (op == '&')
+                    {
+                        stack.Push(list.All(x => x == 't') ? 't' : 'f');
+                    }
+                    else if (op == '|')
+                    {
+                        stack.Push(list.Any(x => x == 't') ? 't' : 'f');
+                    }
+                }
+                else if (c != ',')
+                {
+                    stack.Push(c);
+                }
+            }
+            return stack.Pop() == 't';
+        }
+    }
+}
